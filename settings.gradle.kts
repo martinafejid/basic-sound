fc46f7138a8d2d5b855e42a415f2872e59a35696
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2025-04-10 11:20:48 -0400

    init gradle = "8.13" kotlin = "2.1.20" agp = "8.9.1" kotlinx-coroutines = "1.10.1" kotlinx-browser = "0.3" bcv = "0.17.0" dokka = "2.0.0" kover = "0.9.1" Switched basic-logging to a compileOnly dependency Created apiDump

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "basic-sound"
+include(":basic-sound")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
